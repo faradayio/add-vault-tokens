e5546ed4c59e835fb1c8ffd1cd31d9b739021543
Eric Kidd <git@randomhacks.net>
2015-10-25 20:14:34 -0400

    Implement basic token generation as a library

--- lib/add_vault_tokens.rb
+++ lib/add_vault_tokens.rb
@@ -1,5 +1,28 @@
+require "vault"
 require "add_vault_tokens/version"
 
 module AddVaultTokens
-  # Your code goes here...
+  class << self
+    def connect
+      return if @connected
+      @connected = true
+      Vault.address = ENV.fetch('VAULT_ADDR')
+      Vault.token = ENV.fetch('VAULT_MASTER_TOKEN')
+    end
+
+    def create_token_for(service_name)
+      Vault.auth_token.create(name: service_name, policies: [service_name])
+    end
+
+    def add_tokens_to_apps(app_info, prefix: "")
+      result = Marshal.load(Marshal.dump(app_info))
+      result.each do |app_name, info|
+        token = create_token_for(prefix + app_name)
+        info['environment'] ||= {}
+        info['environment']['VAULT_ADDR'] = ENV.fetch('VAULT_ADDR')
+        info['environment']['VAULT_TOKEN'] = token.auth.client_token
+      end
+      result
+    end
+  end
 end
--- spec/add_vault_tokens_spec.rb
+++ spec/add_vault_tokens_spec.rb
@@ -1,11 +1,67 @@
 require 'spec_helper'
+require 'psych'
 
 describe AddVaultTokens do
+  before do
+    allow(ENV).to receive(:fetch).with('VAULT_ADDR') { 'https://example.com' }
+    allow(ENV).to receive(:fetch).with('VAULT_MASTER_TOKEN') { '123' }
+  end
+
+  def token_double(value='fake_token')
+    double(auth: double(client_token: value))
+  end
+
   it 'has a version number' do
     expect(AddVaultTokens::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'connects to vault using VAULT_ADDR and VAULT_MASTER_TOKEN' do
+    expect(Vault).to receive(:address=).with('https://example.com')
+    expect(Vault).to receive(:token=).with('123')
+    AddVaultTokens.connect
+  end
+
+  it 'can issue a single token' do
+    expect(Vault.auth_token).to receive(:create)
+      .with(name: 'staging-api', policies: ['staging-api'])
+      .and_return(token_double('new_token'))
+
+    result = AddVaultTokens.create_token_for('staging-api')
+    expect(result.auth.client_token).to eq('new_token')
+  end
+
+  it 'can update a JSON structure with appropriate tokens' do
+    # Stub out create_token_for this time, so we don't need to worry about
+    # exactly which arguments it uses.
+    expect(AddVaultTokens).to receive(:create_token_for)
+      .with('staging-app1').and_return(token_double('app1-token'))
+    expect(AddVaultTokens).to receive(:create_token_for)
+      .with('staging-app2').and_return(token_double('app2-token'))
+
+    input = <<YAML
+app1:
+  image: example/app1
+  environment:
+    VAR: "value"
+
+app2:
+  image: example/app2
+YAML
+    output = AddVaultTokens.add_tokens_to_apps(Psych.load(input), prefix: "staging-")
+    expected = <<YAML
+app1:
+  image: example/app1
+  environment:
+    VAR: "value"
+    VAULT_ADDR: "https://example.com"
+    VAULT_TOKEN: "app1-token"
+
+app2:
+  image: example/app2
+  environment:
+    VAULT_ADDR: "https://example.com"
+    VAULT_TOKEN: "app2-token"
+YAML
+    expect(output).to eq(Psych.load(expected))
   end
 end
